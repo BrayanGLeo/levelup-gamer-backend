bb59d84378f9f6078302ddc5e584e57d693df947
Your Name <br.godoys@duocuc.cl>
2025-12-01 05:22:04 -0300

    Habia pasodo a eliminar la clase principal, la cree de nuevo

---
+++ src/main/java/levelup_gamer_backend/LevelupGamerBackendApplication.java
@@ -0,0 +1,13 @@
+package levelup_gamer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupGamerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevelupGamerBackendApplication.class, args);
+    }
+
+}
